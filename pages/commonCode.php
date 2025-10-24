80b4b54aa732668d9dcf1229c2eb28d40907b1a1
AriIo <ariio777@school.lu>
2025-10-24 14:24:35 +0200

    dd

--- pages/commonCode.php
+++ pages/commonCode.php
@@ -1,6 +1,26 @@
 <?php
+$language = "EN";
+if (isset($_GET["lang"])) {
+    $language = $_GET["lang"];
+}
+
+//print("The current language is " . $language);
+
+$arrayOfTranslations = []; 
+
+$fileTranslation = fopen ("translation.csv", "r");
+while (!feof($fileTranslations)){
+    $lineFromFile = fgets($fileTranslation);
+    $piecesOfTranslations = explode (";", $lineFromFile);
+    $arrayOfTranslations[$piecesOfTranslations[0]] = ($language=="EN")?$piecesOfTranslations[1]:$piecesOfTranslations[2];
+}
+
+var_dump($arrayOfTranslations);
+
 function navBar($currentPage)
 {
+    global $language;
+    global $arrayOfTranslations;
     $pages = [
         "Home" => "index.php",
         "All Food" => "food.php",
@@ -17,30 +37,39 @@ function navBar($currentPage)
                         <a href="<?= $url ?>"><?= $name === "Country1" ? "Country #1" : ($name === "Country2" ? "Country #2" : $name) ?></a>
                     </li>
                 <?php endforeach; ?>
+                <form>
+                    <scelect name="lang" onchange="this.form.submit()">
+                        <option value="EN" <?php if ($language == "EN") print "selected"; ?>>English</option>
+                        <option value="GR" <?php if ($language == "GR") print "selected"; ?>>Greek</option>
+                    </scelect>
+                </form>
             </ul>
         </nav>
     </header>
 <?php
 }
+?>
 
-function  userAlreadyRegistered ($checkedUser) {
+<?php
+function  userAlreadyRegistered($checkedUser)
+{
     //this function checks if $checkUser string is an existing use in the Clients.csv
     //IF the given user IS already in the file we will return TRUE0 -> user already registers
     //IF NOT (the user did not register) we wukk return from this function FALSE
 
-    $bReturnValue=false; //the user NOT in our database
+    $bReturnValue = false; //the user NOT in our database
 
     //searching:
-    $fHandler = fopen("Client.csv","r");
-    while (!feof ($fHandler)) {
-        $newLine = fgets ($fHandler);
+    $fHandler = fopen("Client.csv", "r");
+    while (!feof($fHandler)) {
+        $newLine = fgets($fHandler);
         $items = explode(";", $newLine); //gives individual items in string 
-        if ($items [0]==$checkedUser)
+        if ($items[0] == $checkedUser)
             $bReturnValue = true;
     }
     fclose($fHandler);
 
 
-    return $bRetValue;
+    return $bReturnValue;
 }
 ?>
